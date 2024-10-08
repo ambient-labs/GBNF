fa70445409de7de7925a92962882ef5f2d7748de
Ambient Labs <178958084+ambient-labs@users.noreply.github.com>
2024-10-08 13:29:58 -0400

    Abstract out a string GBNF rule

---
+++ packages/gbnf/javascript/src/builder/string-gbnf-rule.ts
@@ -0,0 +1,35 @@
+import {
+  GBNFRule,
+} from "./gbnf-rule.js";
+import { getGBNF, } from "./get-gbnf.js";
+import { getRuleNames, } from "./get-rule-names.js";
+import { getStringValue, } from "./get-string-value.js";
+import {
+  type GrammarBuilder,
+} from "./grammar-builder.js";
+import {
+  type ToStringArgs,
+  type CaseKind,
+} from "./types.js";
+
+type Args = { caseKind?: CaseKind } & ToStringArgs;
+export class StringGBNFRule extends GBNFRule<Args> {
+  getGBNF = (parser: GrammarBuilder, args: Args) => {
+    const {
+      strings,
+      values,
+      separator,
+    } = this;
+
+    const ruleNames = getRuleNames(values, parser, separator, args);
+    const _strings = strings.map(string => {
+      return getStringValue(string, args);
+    });
+    return getGBNF(ruleNames, _strings, {
+      raw: false,
+      wrapped: this.wrapped,
+      separator: this.separator,
+    });
+  };
+
+}
