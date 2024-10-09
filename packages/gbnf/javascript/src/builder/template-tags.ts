52d9b7987429e289cc0fa70fbd4380e5af652cea
Ambient Labs <178958084+ambient-labs@users.noreply.github.com>
2024-10-09 08:58:11 -0400

    Abstract template builder into a static method

--- packages/gbnf/javascript/src/builder/template-tags.ts
+++ packages/gbnf/javascript/src/builder/template-tags.ts
@@ -2,19 +2,6 @@ import { GBNFRule, } from "./gbnf-rule.js";
 import {
   StringGBNFRule,
 } from './string-gbnf-rule.js';
-import { Value, } from "./types.js";
 
-type TemplateTag<GBNFRuleType extends GBNFRule> = {
-  (strings: TemplateStringsArray, ...values: Value[]): GBNFRuleType;
-  key(name: string): (strings: TemplateStringsArray, ...values: Value[]) => GBNFRuleType;
-};
-
-export const $: TemplateTag<StringGBNFRule> = (strings, ...values) => new StringGBNFRule(strings, values);
-export const g: TemplateTag<GBNFRule> = (strings, ...values) => new GBNFRule(strings, values);
-
-$.key = (key) => (strings, ...values) => {
-  return new StringGBNFRule(strings, values, { key: key, });
-};
-g.key = (name) => (strings, ...values) => {
-  return new GBNFRule(strings, values, { key: name, });
-};
+export const g = GBNFRule.templateTag();
+export const $ = StringGBNFRule.templateTag();
