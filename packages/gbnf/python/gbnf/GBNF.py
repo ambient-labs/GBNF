7ff97cc8bfd591cab8c2f347ec89d02972824f00
Ambient Labs <178958084+ambient-labs@users.noreply.github.com>
2025-01-04 15:27:55 -0500

    Update GBNF

--- packages/gbnf/python/gbnf/GBNF.py
+++ packages/gbnf/python/gbnf/GBNF.py
@@ -1,9 +1,12 @@
+from .grammar_graph.graph import Graph
+from .grammar_graph.parse_state import ParseState
+from .grammar_parser.build_rule_stack import build_rule_stack
 from .rules_builder import GrammarParseError, RulesBuilder
 
-ValidInput = str | int | float | list[int | float]
+# ValidInput = str | int | float | list[int | float]
 
 
-def GBNF(grammar: str, _initial_string: ValidInput = ""):
+def GBNF(grammar: str, initial_string: str = ""):
     #   const grammar = typeof input === 'string' ? input : input.toString();
 
     rules_builder = RulesBuilder(grammar)
@@ -12,13 +15,8 @@ def GBNF(grammar: str, _initial_string: ValidInput = ""):
         raise GrammarParseError(grammar, 0, "No rules were found")
     if symbol_ids.get("root") is None:
         raise GrammarParseError(grammar, 0, "Grammar does not contain a 'root' symbol")
-    # root_id = symbol_ids.get("root")
+    root_id: int = symbol_ids["root"]
 
-    # stacked_rules = list(map(build_rule_stack, rules))
-    # graph = Graph(grammar, stacked_rules, root_id)
-    # return ParseState(graph, graph.add(initial_string)
-
-
-#   const stackedRules: UnresolvedRule[][][] = rules.map(buildRuleStack);
-#   const graph = new Graph(grammar, stackedRules, rootId);
-#   return new ParseState(graph, graph.add(initialString));
+    stacked_rules = [build_rule_stack(rule) for rule in rules]
+    graph = Graph(grammar, stacked_rules, root_id)
+    return ParseState(graph, graph.add(initial_string))
